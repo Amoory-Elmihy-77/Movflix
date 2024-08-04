a51fb698a6ab560796404e847328059fb378f4c0
3moory <amaryasser2358@gmail.com>
2024-08-04 16:45:34 +0300

    fix the sound when xlose trailer and edit trailer-container height

--- asstes/js/global.js
+++ asstes/js/global.js
@@ -4,10 +4,11 @@ function showTrail(eid, api_key) {
         .then(data => {
             const trailers = data.results;
             const trailersContainer = document.getElementById('trailers');
-            const trailerDiv = document.querySelector('#trailers .trail-container');
+            const trailerDiv = document.createElement('div');
+            trailerDiv.className = 'trail-container';
             const closeTrail = document.querySelector('#trailers .close i');
             trailersContainer.style.display = 'flex';
-            trailerDiv.innerHTML = '';
+            // trailerDiv.innerHTML = '';
             trailers.forEach(trailer => {
                 if (trailer.type === "Trailer" && trailer.site === "YouTube") {
                     const iframe = document.createElement('iframe');
@@ -22,6 +23,7 @@ function showTrail(eid, api_key) {
             });
             closeTrail.addEventListener("click", () => {
                 trailersContainer.style.display = 'none';
+                trailersContainer.removeChild(trailerDiv);
             })
         })
         .catch(error => console.error('Error fetching the trailers:', error));
