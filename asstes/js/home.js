b953e5194ddf1cec2b774a74dedad96b2840b70f
3moory <amaryasser2358@gmail.com>
2024-08-04 13:42:06 +0300

    finsh home page

--- asstes/js/home.js
+++ asstes/js/home.js
@@ -13,6 +13,12 @@ const searchIcon = document.querySelector('#search .open');
 const searchInput = document.querySelector('#search input');
 const searchClose = document.querySelector('#search .close');
 const header = document.querySelector('header');
+const trending = document.querySelector('.trending .imgs');
+const popMov = document.querySelector('.pop-movies .imgs');
+const popSer = document.querySelector('.pop-series .imgs');
+const topMov = document.querySelector('.top-movies .imgs');
+const topShow = document.querySelector('.top-show .imgs');
+const upcoming = document.querySelector('.upcoming .imgs');
 
 
 get(popular_movies, (data) => {
@@ -23,19 +29,111 @@ get(popular_movies, (data) => {
     document.querySelector('header .overview').innerHTML = overview;
     let img = `${base_url}${backdrop_size}${data_poster}`;
     header.style.backgroundImage = `url(${img})`;
-    });
+});
 
 
 
 searchIcon.addEventListener("click", () => {
-        searchIcon.style.display = 'none';
-        searchClose.style.display = 'block';
-        searchInput.style.display = 'block';
-        searchInput.style.opacity = '1';
-    })
-    searchClose.addEventListener("click", () => {
-        searchIcon.style.display = 'block';
-        searchClose.style.display = 'none';
-        searchInput.style.opacity = '0';
-        searchInput.style.display = 'none';
-    })
+    searchIcon.style.display = 'none';
+    searchClose.style.display = 'block';
+    searchInput.style.display = 'block';
+    searchInput.style.opacity = '1';
+});
+searchClose.addEventListener("click", () => {
+    searchIcon.style.display = 'block';
+    searchClose.style.display = 'none';
+    searchInput.style.opacity = '0';
+    searchInput.style.display = 'none';
+});
+
+
+get(trend_endpoint, (data) => {
+    trending.innerHTML = '';
+    data.results.forEach(element => {
+        let data_poster = element.poster_path;
+        let img = `${base_url}${backdrop_size}${data_poster}`;
+        trending.innerHTML += `
+        <a href="#details.com">
+            <i class="fa-solid fa-play"></i>
+            <img src="${img}" alt="" />
+        </a>
+        `;
+    });
+});
+get(popular_movies, (data) => {
+    popMov.innerHTML = '';
+    data.results.forEach(element => {
+        let data_poster = element.poster_path;
+        let img = `${base_url}${backdrop_size}${data_poster}`;
+        popMov.innerHTML += `
+        <a href="#details.com">
+            <i class="fa-solid fa-play"></i>
+            <img src="${img}" alt="" />
+        </a>
+        `;
+    });
+});
+get(popular_series, (data) => {
+    popSer.innerHTML = '';
+    data.results.forEach(element => {
+        let data_poster = element.poster_path;
+        let img = `${base_url}${backdrop_size}${data_poster}`;
+        popSer.innerHTML += `
+        <a href="#details.com">
+            <i class="fa-solid fa-play"></i>
+            <img src="${img}" alt="" />
+        </a>
+        `;
+    });
+});
+get(top_related, (data) => {
+    topMov.innerHTML = '';
+    data.results.length = 5;
+    let i = 1;
+    data.results.forEach(element => {
+        let data_poster = element.poster_path;
+        let img = `${base_url}${backdrop_size}${data_poster}`;
+        topMov.innerHTML += `
+        <a href="#details.com">
+            <span class="number">${i}</span>
+            <i class="fa-solid fa-play"></i>
+            <img src="${img}" alt="" />
+            <p>${element.name}</p>
+        </a>
+        `;
+        i++;
+    });
+});
+get(top_tvShow, (data) => {
+    topShow.innerHTML = '';
+    data.results.length = 5;
+    let i = 1;
+    data.results.forEach(element => {
+        let data_poster = element.poster_path;
+        let img = `${base_url}${backdrop_size}${data_poster}`;
+        topShow.innerHTML += `
+        <a href="#details.com">
+            <span class="number">${i}</span>
+            <i class="fa-solid fa-play"></i>
+            <img src="${img}" alt="" />
+            <p>${element.name}</p>
+        </a>
+        `;
+        i++;
+    });
+});
+
+get(upcoming_movies, (data) => {
+    upcoming.innerHTML = '';
+    data.results.length = 4;
+    data.results.forEach(element => {
+        let data_poster = element.poster_path;
+        let img = `${base_url}${backdrop_size}${data_poster}`;
+        upcoming.innerHTML += `
+        <a>
+            <i class="fa-solid fa-play"></i>
+            <img src="${img}" alt="" />
+        </a>
+        `;
+    });
+});
